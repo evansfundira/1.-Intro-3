9b02e9c76b392562468fbccf7740842f6dc3f0e5
evansfundira <evansfundira@gmail.com>
2023-10-02 14:34:46 +0200

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,186 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Button1 = New Button()
+        CheckBox1 = New CheckBox()
+        Label1 = New Label()
+        ComboBox1 = New ComboBox()
+        RadioButton1 = New RadioButton()
+        TextBox1 = New TextBox()
+        Label2 = New Label()
+        TextBox2 = New TextBox()
+        Label3 = New Label()
+        RadioButton2 = New RadioButton()
+        RadioButton3 = New RadioButton()
+        CheckBox2 = New CheckBox()
+        SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(330, 305)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(94, 29)
+        Button1.TabIndex = 0
+        Button1.Text = "Click Here"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' CheckBox1
+        ' 
+        CheckBox1.AutoSize = True
+        CheckBox1.Location = New Point(330, 232)
+        CheckBox1.Name = "CheckBox1"
+        CheckBox1.Size = New Size(95, 24)
+        CheckBox1.TabIndex = 1
+        CheckBox1.Text = "Subscribe"
+        CheckBox1.UseVisualStyleBackColor = True
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(179, 71)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(49, 20)
+        Label1.TabIndex = 2
+        Label1.Text = "Name"
+        ' 
+        ' ComboBox1
+        ' 
+        ComboBox1.FormattingEnabled = True
+        ComboBox1.Items.AddRange(New Object() {"Teacher", "Farmer", "Student", "Unemployed"})
+        ComboBox1.Location = New Point(357, 182)
+        ComboBox1.Name = "ComboBox1"
+        ComboBox1.Size = New Size(151, 28)
+        ComboBox1.TabIndex = 3
+        ' 
+        ' RadioButton1
+        ' 
+        RadioButton1.AutoSize = True
+        RadioButton1.Location = New Point(599, 120)
+        RadioButton1.Name = "RadioButton1"
+        RadioButton1.Size = New Size(67, 24)
+        RadioButton1.TabIndex = 4
+        RadioButton1.TabStop = True
+        RadioButton1.Text = "Other"
+        RadioButton1.UseVisualStyleBackColor = True
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(357, 64)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(125, 27)
+        TextBox1.TabIndex = 5
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(179, 124)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(67, 20)
+        Label2.TabIndex = 2
+        Label2.Text = "Surname"
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(357, 117)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(125, 27)
+        TextBox2.TabIndex = 5
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(179, 185)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(77, 20)
+        Label3.TabIndex = 2
+        Label3.Text = "Profession"
+        ' 
+        ' RadioButton2
+        ' 
+        RadioButton2.AutoSize = True
+        RadioButton2.Location = New Point(599, 90)
+        RadioButton2.Name = "RadioButton2"
+        RadioButton2.Size = New Size(78, 24)
+        RadioButton2.TabIndex = 4
+        RadioButton2.TabStop = True
+        RadioButton2.Text = "Female"
+        RadioButton2.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton3
+        ' 
+        RadioButton3.AutoSize = True
+        RadioButton3.Location = New Point(599, 60)
+        RadioButton3.Name = "RadioButton3"
+        RadioButton3.Size = New Size(63, 24)
+        RadioButton3.TabIndex = 4
+        RadioButton3.TabStop = True
+        RadioButton3.Text = "Male"
+        RadioButton3.UseVisualStyleBackColor = True
+        ' 
+        ' CheckBox2
+        ' 
+        CheckBox2.AutoSize = True
+        CheckBox2.Location = New Point(330, 262)
+        CheckBox2.Name = "CheckBox2"
+        CheckBox2.Size = New Size(106, 24)
+        CheckBox2.TabIndex = 1
+        CheckBox2.Text = "Sign Me up"
+        CheckBox2.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(TextBox2)
+        Controls.Add(TextBox1)
+        Controls.Add(RadioButton3)
+        Controls.Add(RadioButton2)
+        Controls.Add(RadioButton1)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(ComboBox1)
+        Controls.Add(Label1)
+        Controls.Add(CheckBox2)
+        Controls.Add(CheckBox1)
+        Controls.Add(Button1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents RadioButton3 As RadioButton
+    Friend WithEvents CheckBox2 As CheckBox
+End Class
